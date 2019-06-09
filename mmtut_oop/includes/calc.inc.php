42a3c98772b0eab91a9da7508e161f6e5245a848
Ajayi Dhikrullah <ajayidhikrullah@gmail.com>
2019-06-09 14:01:10 +0100

    built a simple add, subtract and multiply calculator with raw php OOP,

---
+++ mmtut_oop/includes/calc.inc.php
@@ -0,0 +1,39 @@
+<?php
+class Calc{
+	public $num1;
+	public $num2;
+	public $cal;
+
+	public function __construct($num1, $num2, $cal){
+		var_dump(func_get_args());
+		$this->num1 = $num1;
+		$this->num2 = $num2;
+		$this->cal = $cal;
+	}
+
+	////calculaate method should be the next
+	public function calcFunction(){
+		switch ($this->cal){
+			case 'add':
+				$result = $this->num1 + $this->num2;
+				break;
+
+			case 'sub':
+				$result = $this->num1 - $this->num2;
+				break;
+			case 'mul':
+				$result = $this->num1 * $this->num2;
+				break;
+			default:
+				$result = 'Error';
+				break;
+		}
+
+		return "$result is the result";
+	}
+}
+
+
+
+
+?>
--- mmtut_oop/index.php
+++ mmtut_oop/index.php
@@ -5,13 +5,30 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<title>Document</title>
+	<link rel="stylesheet" href="bootstrap.min.css">
 </head>
 <body>
-	<form action="calc.php" method="POST">
+	<div class="container-fluid">
 		
-	
-	
-	
-	</form>
+		<form class="form-group" action="calc.php" method="POST">
+		<div class="form-control">
+			<label for="num1">Number1</label>
+			<input type="text" name="num1">
+		</div>
+		<button class="btn btn-primary" type="submit">Calculate</button>		
+
+		
+		<div class="form-control">
+			<label for="num2">Number2</label>
+			<input type="text" name="num2">
+		</div>
+			<select name="cal" id="">
+				<option value="add">Add</option>
+				<option value="sub">Subtract</option>
+				<option value="mul">Multiply</option>
+			</select>
+		</form>
+	</div>
+
 </body>
 </html>
